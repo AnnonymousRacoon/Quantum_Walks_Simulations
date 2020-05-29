a154ad2301da451a0623423364b2cf238c940617
qwchagas <60764942+qwchagas@users.noreply.github.com>
2020-05-29 15:26:58 -0300

    Delete CompleteAdjacency.m

--- ctqw - complete graph + searching/CompleteAdjacency.m
+++
@@ -1,7 +0,0 @@
-function [adjacency] = CompleteAdjacency(number_of_nodes)
-%CompleteAdjacency 
-% What's going on: adjacency matrix of a complete graph
-adjacency = ones(number_of_nodes) - eye(number_of_nodes);
-
-end
-
