2a132dc6e7222a13fd4ad123c5fbcc0d4d5324dc
Bruno Chagas <60764942+qwchagas@users.noreply.github.com>
2020-06-15 16:49:37 -0300

    Update HadamardCoin.m

--- coined - complete bipartite flip-flop + searching/HadamardCoin.m
+++ coined - complete bipartite flip-flop + searching/HadamardCoin.m
@@ -1,6 +1,5 @@
 function [operator] = HadamardCoin(degree)
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
+
 operator = zeros(2*degree^2);
 hadamard = [1,1;1,-1]/sqrt(2); 
 coin = hadamard;
