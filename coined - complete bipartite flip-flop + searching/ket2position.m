865f4aab27567d353dc1f369db5eeb4a97574735
Bruno Chagas <60764942+qwchagas@users.noreply.github.com>
2020-06-15 16:50:03 -0300

    Update ket2position.m

--- coined - complete bipartite flip-flop + searching/ket2position.m
+++ coined - complete bipartite flip-flop + searching/ket2position.m
@@ -1,6 +1,5 @@
 function [pos] = ket2position(position,dimensions)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+
 pos = 0;
 for x=1:length(dimensions)
     pos = pos + position(x)*prod(dimensions(x+1:end));
