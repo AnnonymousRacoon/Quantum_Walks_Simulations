b62c5d12ac418c4ce19af07f6e0bc1fde492e573
Bruno Chagas <brunochagas@gmail.com>
2020-08-12 14:26:20 -0300

    Create SearchingEvolution.m

---
+++ continuous - circulant graphs + searching/SearchingEvolution.m
@@ -0,0 +1,17 @@
+function [searching_probability] = SearchingEvolution(adjacency,gamma,initial_state,marked_elements,time_array)
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+
+adjacency = adjacency*gamma;
+for x = marked_elements
+    adjacency(x+1,x+1) = 1;
+end
+
+index = 1;
+for t = time_array
+    final_state = expm(1i*t*adjacency)*initial_state;
+    searching_probability(index) = sum(final_state(marked_elements+1).*conj(final_state(marked_elements+1)));
+    index = index + 1;
+end
+
+end
